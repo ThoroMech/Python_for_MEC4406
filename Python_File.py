1befbd9f980f72d33ef761fe05bb3f8e061f16f7
ThoroMech <alistair1@live.com.au>
2022-07-31 17:01:26 +1000

    Commit

---
+++ Python_File.py
@@ -0,0 +1,21 @@
+"""
+Created on Sun Jul 31 16:46:21 2022
+Created By Alistair Thorogood
+July 2022
+for MEC4406 Week 1
+"""
+
+#Initialise variables
+i = 0
+favNum = 16
+
+# Write in python that prints your name
+print ("Alistair Thorogood")
+
+#count to favourite number squared and print
+for i in range (0, favNum**2):
+    i += 1
+    
+print ("My favourite number squred =",i)
+
+
